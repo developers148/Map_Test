95c9fbbad849263f1913274cb55d0bf2b9170aaa
Irfan hossain <32560686+toumalmojumder@users.noreply.github.com>
2020-02-26 15:03:21 +0600

    Initial commit

---
+++ app/src/main/java/com/leads/maptest/MainActivity.java
@@ -0,0 +1,368 @@
+package com.leads.maptest;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.PointF;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.mapbox.geojson.Feature;
+import com.mapbox.geojson.FeatureCollection;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.annotations.BubbleLayout;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
+import com.mapbox.mapboxsdk.style.layers.SymbolLayer;
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
+
+import java.io.InputStream;
+import java.lang.ref.WeakReference;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.List;
+
+import static com.mapbox.mapboxsdk.style.expressions.Expression.eq;
+import static com.mapbox.mapboxsdk.style.expressions.Expression.get;
+import static com.mapbox.mapboxsdk.style.expressions.Expression.literal;
+import static com.mapbox.mapboxsdk.style.layers.Property.ICON_ANCHOR_BOTTOM;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconAllowOverlap;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconAnchor;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconImage;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconOffset;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MapView mapView;
+    private MapboxMap mapbox;
+    private FeatureCollection featureCollection;
+    private GeoJsonSource source;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Mapbox.getInstance(this, getString(R.string.mapbox_access_token));
+        setContentView(R.layout.activity_main);
+
+
+        mapView = findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull final MapboxMap mapboxMap) {
+                mapbox = mapboxMap;
+                mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
+                    @Override
+                    public void onStyleLoaded(@NonNull Style style) {
+
+// Map is set up and the style has loaded. Now you can add data or make other map adjustments.
+
+
+                    }
+                });
+
+                new LoadGeoJsonDataTask(MainActivity.this).execute();
+
+                mapboxMap.addOnMapClickListener(new MapboxMap.OnMapClickListener() {
+                    @Override
+                    public boolean onMapClick(@NonNull LatLng point) {
+
+                        return handleClickIcon(mapboxMap.getProjection().toScreenLocation(point));
+                    }
+                });
+            }
+
+
+        });
+
+        mapView.onCreate(savedInstanceState);
+
+    }
+
+
+
+    private boolean handleClickIcon(PointF screenPoint) {
+        List<Feature> features = mapbox.queryRenderedFeatures(screenPoint, "MARKER_LAYER_ID");
+        if (!features.isEmpty()) {
+            String name = features.get(0).getStringProperty("name");
+            List<Feature> featureList = featureCollection.features();
+            if (featureList != null) {
+                for (int i = 0; i < featureList.size(); i++) {
+                    if (featureList.get(i).getStringProperty("name").equals(name)) {
+                        if (featureSelectStatus(i)) {
+                            setFeatureSelectState(featureList.get(i), false);
+                        } else {
+                            setSelected(i);
+                        }
+                    }
+                }
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void setUpData(final FeatureCollection collection) {
+        featureCollection = collection;
+        if (mapbox != null) {
+            mapbox.getStyle(new Style.OnStyleLoaded() {
+                @Override
+                public void onStyleLoaded(@NonNull Style style) {
+                    MainActivity.this.setupSource(style);
+                    MainActivity.this.setUpImage(style);
+                    MainActivity.this.setUpMarkerLayer(style);
+                    MainActivity.this.setUpInfoWindowLayer(style);
+                }
+            });
+        }
+    }
+    private boolean featureSelectStatus(int index) {
+        if (featureCollection == null) {
+            return false;
+        }
+        return featureCollection.features().get(index).getBooleanProperty("selected");
+    }
+
+
+    private void setFeatureSelectState(Feature feature, boolean selectedState) {
+        if (feature.properties() != null) {
+            feature.properties().addProperty("selected", selectedState);
+            refreshSource();
+        }
+    }
+
+    private void setSelected(int index) {
+        if (featureCollection.features() != null) {
+            Feature feature = featureCollection.features().get(index);
+            setFeatureSelectState(feature, true);
+            refreshSource();
+        }
+    }
+
+    private void refreshSource() {
+        if (source != null && featureCollection != null) {
+            source.setGeoJson(featureCollection);
+        }
+    }
+    private void setupSource(@NonNull Style loadedStyle) {
+        source = new GeoJsonSource("GEOJSON_SOURCE_ID", featureCollection);
+        loadedStyle.addSource(source);
+    }
+    private void setUpImage(@NonNull Style loadedStyle) {
+        loadedStyle.addImage("MARKER_IMAGE_ID", BitmapFactory.decodeResource(
+                this.getResources(), R.drawable.red_marker));
+    }
+    private void setUpMarkerLayer(@NonNull Style loadedStyle) {
+        loadedStyle.addLayer(new SymbolLayer("MARKER_LAYER_ID", "GEOJSON_SOURCE_ID")
+                .withProperties(
+                        iconImage("MARKER_IMAGE_ID"),
+                        iconAllowOverlap(true),
+                        iconOffset(new Float[] {0f, -8f})
+                ));
+    }
+
+    private void setUpInfoWindowLayer(@NonNull Style loadedStyle) {
+        loadedStyle.addLayer(new SymbolLayer("CALLOUT_LAYER_ID", "GEOJSON_SOURCE_ID")
+                .withProperties(
+                        /* show image with id title based on the value of the name feature property */
+                        iconImage("{name}"),
+
+                        /* set anchor of icon to bottom-left */
+                        iconAnchor(ICON_ANCHOR_BOTTOM),
+
+                        /* all info window and marker image to appear at the same time*/
+                        iconAllowOverlap(true),
+
+                        /* offset the info window to be above the marker */
+                        iconOffset(new Float[] {-2f, -28f})
+                )
+/* add a filter to show only when selected feature property is true */
+                .withFilter(eq((get("selected")), literal(true))));
+    }
+
+
+
+
+
+
+
+    private static class LoadGeoJsonDataTask extends AsyncTask<Void, Void, FeatureCollection> {
+
+        private final WeakReference<MainActivity> activityRef;
+
+        LoadGeoJsonDataTask(MainActivity activity) {
+            this.activityRef = new WeakReference<>(activity);
+        }
+
+        @Override
+        protected FeatureCollection doInBackground(Void... params) {
+            MainActivity activity = activityRef.get();
+
+            if (activity == null) {
+                return null;
+            }
+
+            String geoJson = loadGeoJsonFromAsset(activity, "us_west_coast.geojson");
+            return FeatureCollection.fromJson(geoJson);
+        }
+
+        @Override
+        protected void onPostExecute(FeatureCollection featureCollection) {
+            super.onPostExecute(featureCollection);
+            MainActivity activity = activityRef.get();
+            if (featureCollection == null || activity == null) {
+                return;
+            }
+
+// This example runs on the premise that each GeoJSON Feature has a "selected" property,
+// with a boolean value. If your data's Features don't have this boolean property,
+// add it to the FeatureCollection 's features with the following code:
+            for (Feature singleFeature : featureCollection.features()) {
+                singleFeature.addBooleanProperty("selected", false);
+            }
+
+            Log.e("featurecollection",featureCollection.toString());
+            activity.setUpData(featureCollection);
+            new GenerateViewIconTask(activity).execute(featureCollection);
+        }
+
+        static String loadGeoJsonFromAsset(Context context, String filename) {
+            try {
+// Load GeoJSON file from local asset folder
+                InputStream is = context.getAssets().open(filename);
+                int size = is.available();
+                byte[] buffer = new byte[size];
+                is.read(buffer);
+                is.close();
+                return new String(buffer, StandardCharsets.UTF_8);
+            } catch (Exception exception) {
+                throw new RuntimeException(exception);
+            }
+        }
+    }
+
+
+    private static class GenerateViewIconTask extends AsyncTask<FeatureCollection, Void, HashMap<String, Bitmap>> {
+
+        private final HashMap<String, View> viewMap = new HashMap<>();
+        private final WeakReference<MainActivity> activityRef;
+        private final boolean refreshSource;
+
+        GenerateViewIconTask(MainActivity activity, boolean refreshSource) {
+            this.activityRef = new WeakReference<>(activity);
+            this.refreshSource = refreshSource;
+        }
+
+        GenerateViewIconTask(MainActivity activity) {
+            this(activity, false);
+        }
+
+        @SuppressWarnings("WrongThread")
+        @Override
+        protected HashMap<String, Bitmap> doInBackground(FeatureCollection... params) {
+            MainActivity activity = activityRef.get();
+            if (activity != null) {
+                HashMap<String, Bitmap> imagesMap = new HashMap<>();
+                LayoutInflater inflater = LayoutInflater.from(activity);
+
+                FeatureCollection featureCollection = params[0];
+
+                for (Feature feature : featureCollection.features()) {
+
+                    BubbleLayout bubbleLayout = (BubbleLayout)
+                            inflater.inflate(R.layout.symbol_layer_info_window_layout_callout, null);
+
+                    String name = feature.getStringProperty("name");
+                    TextView titleTextView = bubbleLayout.findViewById(R.id.info_window_title);
+                    titleTextView.setText(name);
+
+                    String style = feature.getStringProperty("capital");
+                    TextView descriptionTextView = bubbleLayout.findViewById(R.id.info_window_description);
+                    descriptionTextView.setText(
+                            String.format(activity.getString(R.string.capital), style));
+
+                    int measureSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
+                    bubbleLayout.measure(measureSpec, measureSpec);
+
+                    float measuredWidth = bubbleLayout.getMeasuredWidth();
+
+                    bubbleLayout.setArrowPosition(measuredWidth / 2 - 5);
+
+                    Bitmap bitmap = SymbolGenerator.generate(bubbleLayout);
+                    imagesMap.put(name, bitmap);
+                    viewMap.put(name, bubbleLayout);
+                }
+
+                return imagesMap;
+            } else {
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(HashMap<String, Bitmap> bitmapHashMap) {
+            super.onPostExecute(bitmapHashMap);
+            MainActivity activity = activityRef.get();
+            if (activity != null && bitmapHashMap != null) {
+                activity.setImageGenResults(bitmapHashMap);
+                if (refreshSource) {
+                    activity.refreshSource();
+                }
+            }
+            Toast.makeText(activity, "Tap on a marker", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private static class SymbolGenerator {
+
+        /**
+         * Generate a Bitmap from an Android SDK View.
+         *
+         * @param view the View to be drawn to a Bitmap
+         * @return the generated bitmap
+         */
+        static Bitmap generate(@NonNull View view) {
+            int measureSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
+            view.measure(measureSpec, measureSpec);
+
+            int measuredWidth = view.getMeasuredWidth();
+            int measuredHeight = view.getMeasuredHeight();
+
+            view.layout(0, 0, measuredWidth, measuredHeight);
+            Bitmap bitmap = Bitmap.createBitmap(measuredWidth, measuredHeight, Bitmap.Config.ARGB_8888);
+            bitmap.eraseColor(Color.TRANSPARENT);
+            Canvas canvas = new Canvas(bitmap);
+            view.draw(canvas);
+            return bitmap;
+        }
+    }
+
+    public void setImageGenResults(final HashMap<String, Bitmap> imageMap) {
+        if (mapbox != null) {
+            mapbox.getStyle(new Style.OnStyleLoaded() {
+                @Override
+                public void onStyleLoaded(@NonNull Style style) {
+// calling addImages is faster as separate addImage calls for each bitmap.
+                    style.addImages(imageMap);
+                }
+            });
+        }
+    }
+
+}
